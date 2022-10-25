06bcae19eb8ca110d477ded982a3d3751c077e43
Mirek Skála <mirek.skala@seznam.cz>
2022-10-25 10:40:18 +0200

    Created Welcome Text (+Js), started Insert Words Inputs

---
+++ script.js
@@ -0,0 +1,25 @@
+/**** Welcome Text ****/
+
+const heading = document.getElementById("text");
+
+const text = "Procvičování cizích slov";
+
+let idLetter = 1;
+
+let delay = 150;
+
+function printText(){
+    heading.innerText = text.slice(0,idLetter)
+
+    idLetter++;
+
+    if(idLetter > text.length){
+        idLetter = 1;
+    }
+
+    setTimeout(printText, delay);
+}
+
+printText();
+
+/**** Insert Words Btn ****/
