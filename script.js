e22690bcbbf4b54b946e236b4e991758fa37ae20
Mirek Skála <mirek.skala@seznam.cz>
2022-10-27 10:08:51 +0200

    Inserting values from inputs to arrays (Js)

--- script.js
+++ script.js
@@ -31,22 +31,49 @@ for( let i = 1; i <= 3; i++ ) {
     let window = document.getElementById("window" + i);
 
     btn.onclick = function(){
-    btn.style.display = "none";
+        btn.style.display = "none";
 
-    if(btn.style.display == "none") {
-        window.style.display = "block";
-    }
+        if(btn.style.display == "none") {
+            window.style.display = "block";
+        }
 
-    /**** Cross Fce ****/
+        /**** Cross Fce ****/
 
-    let cross = document.getElementById("cross" + i);
+        let cross = document.getElementById("cross" + i);
 
-    cross.onclick = function(){
-        window.style.display = "none";
+        cross.onclick = function(){
+            window.style.display = "none";
 
-        if(window.style.display == "none") {
-            btn.style.display = "block";
+            if(window.style.display == "none") {
+                btn.style.display = "block";
+            }
         }
     }
 }
-}
+
+/**** Input Values ****/
+
+let btn1 = document.getElementById("btn_insert_submit");
+
+let input_foreign = document.getElementById("foreign_word");
+
+let input_native = document.getElementById("native_word");
+
+let foreign_word = [];
+
+let native_word = [];
+
+btn1.addEventListener("click", function(){
+    /* Add a foreign word to array */
+    foreign_word.push(input_foreign.value);
+    console.log(foreign_word);
+    input_foreign.value = "";
+
+    /* Add a native word to array */
+    native_word.push(input_native.value);
+    console.log(native_word);
+    input_native.value = "";
+});
+
+
+/**** First Practice ****/
