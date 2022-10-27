44548a9abe0295e573bc2d4a1de53effea22af10
Mirek Skála <mirek.skala@seznam.cz>
2022-10-27 14:00:57 +0200

    Test solved

--- script.js
+++ script.js
@@ -59,63 +59,37 @@ let input_foreign = document.getElementById("foreign_word");
 
 let input_native = document.getElementById("native_word");
 
-let foreign_word = [];
-
-let native_word = [];
-
+if(localStorage.getItem("practice") == null) {
+    var practice_array = [];
+}   else {
+    practice_array = JSON.parse(localStorage.getItem("practice"));
+}
 
 btn1.addEventListener("click", function(){
-    /* Add a foreign word to array */
-    foreign_word.push(input_foreign.value);
-    console.log(foreign_word);
-    /* Add to Local Storage */
-    let foreign_word_ToLS = JSON.stringify(foreign_word);
-    localStorage.setItem("Foreign Words", foreign_word_ToLS);
-    input_foreign.value = "";
-    let foreign_word_FromLS = JSON.parse(foreign_word_ToLS);
-
-    /* Add a native word to array */
-    native_word.push(input_native.value);
-    console.log(native_word);
-    /* Add to Local Storage */
-    let native_word_ToLS = JSON.stringify(native_word);
-    localStorage.setItem("Native Words", native_word_ToLS);
-    input_native.value = "";
-    let native_word_FromLS = JSON.parse(native_word_ToLS);  
-
-    /* Could be native_word_FromLS.length */
-
-    var array_length = foreign_word_FromLS.length;   
-
-    /**** First Practice ****/
-
-    /* Randomize Function */
-
-    let btn_randomize = document.getElementById("randomize_btn");
-
-    let random_foreign = document.getElementsByClassName("random_foreign_word");
-
-    let random_native = document.getElementsByClassName("random_native_word");
-
-    btn_randomize.addEventListener("click", function(){
-        /* Generate Random Number */
-        /*let random_number = Math.ceil(Math.random() * array_length);   
-        /* Show random word from array */
-        /*random_foreign.innerHTML = foreign_word_FromLS[random_number];
-        /* Show random word from array */
-        /*random_native.innerHTML = native_word_FromLS[random_number];*/
-        for( let x = 0; x < array_length; x++ ) {
-            
-        }
+    
+    practice_array.push({
+        foreign: foreign_word.value,
+        native: native_word.value
     });
 
+    var practice_arrayJSON = JSON.stringify(practice_array);
+    localStorage.setItem("practice", practice_arrayJSON);
+
+    foreign_word.value = "";
+    native_word.value = "";
 });
 
 
 /**** First Practice ****/
 
 /* Randomize Function */
+
+/* Could be native_word_FromLS.length */
 /*
+var array_length = foreign_word_FromLS.length;   
+
+console.log(array_length);
+
 let btn_randomize = document.getElementById("randomize_btn");
 
 let random_foreign = document.getElementsByClassName("random_foreign_word");
@@ -123,9 +97,10 @@ let random_foreign = document.getElementsByClassName("random_foreign_word");
 let random_native = document.getElementsByClassName("random_native_word");
 
 btn_randomize.addEventListener("click", function(){
-    /* Generate Random Number *//*
-    let random_number = Math.ceil(Math.random() * array_length);   
+    /* Generate Random Number */
+    /*let random_number = Math.ceil(Math.random() * array_length);   
     /* Show random word from array */
     /*console.log(random_number);
 
 });*/
+
