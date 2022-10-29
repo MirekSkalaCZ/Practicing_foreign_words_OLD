dbf1b58652ec24909a8e459f505c3d2d86d26ba6
Mirek Skála <mirek.skala@seznam.cz>
2022-10-29 18:26:41 +0200

    Edited: 1.04 version

--- english_version/script_en.js
+++ english_version/script_en.js
@@ -9,20 +9,20 @@ if(localStorage.getItem("theme") == null){
 let localData = localStorage.getItem("theme");
 
 if(localData == "light") {
-    icon_dm.src = "images/moon.png";
+    icon_dm.src = "../images/moon.png";
     document.body.classList.remove("dark_theme");
 }   else if (localData == "dark") {
-    icon_dm.src = "images/sun.png";
+    icon_dm.src = "../images/sun.png";
     document.body.classList.add("dark_theme");
 }
 
 icon_dm.addEventListener ("click", function(){
     document.body.classList.toggle("dark_theme");
     if(document.body.classList.contains("dark_theme")){
-        icon_dm.src = "images/sun.png";
+        icon_dm.src = "../images/sun.png";
         localStorage.setItem("theme", "dark");
     }   else {
-        icon_dm.src = "images/moon.png";
+        icon_dm.src = "../images/moon.png";
         localStorage.setItem("theme", "light");
     }
 });
